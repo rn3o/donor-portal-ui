c770ec10d9c54e08fa8a538e925393be75aa363a
rn3o <ryan.sael@n3o.ltd>
2024-05-08 15:32:59 +0100

    nocheck

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { GlobalProvider } from './GlobalProvider';
 import GlobalStateToggles from './GlobalStateToggles';
 import App from './App';
 
-const root = createRoot(document.getElementById('root'));
+// const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root')!);
+
 
 const AppWrapper = () => {
   return (
